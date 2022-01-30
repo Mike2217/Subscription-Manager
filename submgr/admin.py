f1c84ac910e911ea5932d092a26632e74fee3a17
Mike2217 <msoles2217@gmail.com>
2022-01-30 16:15:31 -0500

    backend

--- submgr/admin.py
+++ submgr/admin.py
@@ -1,8 +1,7 @@
 from ast import Sub
 from re import S
 from django.contrib import admin
-from .models import User, Subscriptions
+from .models import Subscriptions
 # Register your models here.
 
-admin.site.register(User)
 admin.site.register(Subscriptions)
